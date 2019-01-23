000a838f413643a8b2cf52aac3c83bde13ee0ae6
Play Easy Now <16202860+playeasynow@users.noreply.github.com>
2019-01-23 15:09:32 -0600

    removed components of examples

--- client/src/views/Components/Components.jsx
+++ client/src/views/Components/Components.jsx
@@ -15,18 +15,18 @@ import Button from "components/CustomButtons/Button.jsx";
 import Parallax from "components/Parallax/Parallax.jsx";
 // sections for this page
 import HeaderLinks from "components/Header/HeaderLinks.jsx";
-import SectionBasics from "./Sections/SectionBasics.jsx";
-import SectionNavbars from "./Sections/SectionNavbars.jsx";
-import SectionTabs from "./Sections/SectionTabs.jsx";
-import SectionPills from "./Sections/SectionPills.jsx";
-import SectionNotifications from "./Sections/SectionNotifications.jsx";
-import SectionTypography from "./Sections/SectionTypography.jsx";
-import SectionJavascript from "./Sections/SectionJavascript.jsx";
+// import SectionBasics from "./Sections/SectionBasics.jsx";
+// import SectionNavbars from "./Sections/SectionNavbars.jsx";
+// import SectionTabs from "./Sections/SectionTabs.jsx";
+// import SectionPills from "./Sections/SectionPills.jsx";
+// import SectionNotifications from "./Sections/SectionNotifications.jsx";
+// import SectionTypography from "./Sections/SectionTypography.jsx";
+// import SectionJavascript from "./Sections/SectionJavascript.jsx";
 import SectionCarousel from "./Sections/SectionCarousel.jsx";
 import SectionCompletedExamples from "./Sections/SectionCompletedExamples.jsx";
 import SectionLogin from "./Sections/SectionLogin.jsx";
 import SectionExamples from "./Sections/SectionExamples.jsx";
-import SectionDownload from "./Sections/SectionDownload.jsx";
+// import SectionDownload from "./Sections/SectionDownload.jsx";
 
 import componentsStyle from "assets/jss/material-kit-react/views/components.jsx";
 
@@ -62,13 +62,13 @@ class Components extends React.Component {
         </Parallax>
 
         <div className={classNames(classes.main, classes.mainRaised)}>
-          <SectionBasics />
+          {/* <SectionBasics />
           <SectionNavbars />
           <SectionTabs />
           <SectionPills />
           <SectionNotifications />
-          <SectionTypography />
-          <SectionJavascript />
+          <SectionTypography /> */}
+          {/* <SectionJavascript /> */}
           <SectionCarousel />
           <SectionCompletedExamples />
           <SectionLogin />
@@ -80,7 +80,7 @@ class Components extends React.Component {
             </Link>
           </GridItem>
           <SectionExamples />
-          <SectionDownload />
+          {/* <SectionDownload /> */}
         </div>
         <Footer />
       </div>
