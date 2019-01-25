d4caf5f04a6d0e38109b66941ea1a8754c107cb9
Play Easy Now <16202860+playeasynow@users.noreply.github.com>
2019-01-25 16:43:55 -0600

    removed material UI, added animated background

---
+++ client/src/components/Nav/index.js
@@ -0,0 +1,68 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import "./style.css";
+
+class Nav extends Component {
+    state = {
+        open: false,
+        width: window.innerWidth
+    };
+
+    updateWidth = () => {
+        const newState = { width: window.innerWidth };
+
+        if (this.state.open && newState.width > 991) {
+            newState.open = false;
+        }
+        this.setState(newState);
+    };
+
+    toggleNav = () => {
+        this.setState({ open: !this.state.open });
+    };
+
+    componentDidMount() {
+        window.addEventListener("resize", this.updateWidth);
+    }
+
+    componentWillUnMount() {
+        window.removeEventListener("resize", this.updateWidth);
+    }
+
+    render() {
+        return (
+            <nav className="navbar navbar-expand-lg navbar-light">
+                <Link className="navbar-brand text-dark" to="/">
+                <h2><i><b>Guru Match</b></i></h2>
+                </Link>
+                <button
+                    onClick={this.toggleNav}
+                    className="navbar-toggler"
+                    data-toggle="collapse"
+                    data-target="#navbarNav"
+                    aria-controls="navbarNav"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                    >
+                        <span className="navbar-toggler-icon" />
+                    </button>
+                    <div className={`${this.state.open ? "" : "collapse "}navbar-collapse navBtn`} id="navbarNav">
+                        <ul className="navbar-item list-unstyled">
+                            <li className="nav-item mt-3">
+                                <Link
+                                onClick={this.toggleNav}
+                                className={window.location.pethname === "/" ? "nav-link active" : "nav-link"}
+                                to="/saved"
+                                >
+                                Saved Books
+                                </Link>
+                            </li>
+                        </ul>
+                    </div>
+            </nav>
+
+        );
+    }
+}
+
+export default Nav;
