cd8aa8a4797b220da4f2566f3faaca3453e7d10b
Play Easy Now <16202860+playeasynow@users.noreply.github.com>
2019-01-31 16:34:55 -0600

    added sign up page and styled it

--- client/src/components/SignUp/index.js
+++ client/src/components/SignUp/index.js
@@ -1,24 +1,141 @@
-import React, { Component } from "react";
-import Jumbotron from "../Jumbotron";
-import { Col, Row, Container } from "../Grid";
+import React, { Component } from 'react';
+import { Link, withRouter } from 'react-router-dom';
+import { compose } from 'recompose';
+import "./style.css";
+import { withFirebase } from '../Firebase';
+import * as ROUTES from '../../constants/routes';
 
-class SignUp extends Component {
+const SignUpPage = () => (
+    <div className="container-fluid" id="sign-up-bg">
+        <div className="container" id="sign-up-box">
+            <h1 className="lobster-font">create an account</h1><br></br>
+            <SignUpForm />
+        </div>
+    </div>
+);
+
+const INITIAL_STATE = {
+    username: '',
+    email: '',
+    passwordOne: '',
+    passwordTwo: '',
+    error: null,
+};
+
+class SignUpFormBase extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { ...INITIAL_STATE };
+    };
+
+    onSubmit = event => {
+        const { email, passwordOne } = this.state;
+
+        this.props.firebase
+            .doCreateUserWithEmailAndPassword(email, passwordOne)
+            .then(authUser => {
+                this.setState({ ...INITIAL_STATE });
+                this.props.history.push(ROUTES.SURVEY);
+            })
+            .catch(error => {
+                this.setState({ error });
+            });
+
+        event.preventDefault();
+
+    }
+
+    onChange = event => {
+        this.setState({ [event.target.name]: event.target.value });
+    };
 
     render() {
+        const {
+            username,
+            email,
+            passwordOne,
+            passwordTwo,
+            error,
+        } = this.state;
+
+        const isInvalid =
+            passwordOne !== passwordTwo ||
+            passwordOne === '' ||
+            email === '' ||
+            username === '';
+
         return (
-            <Container>
-                <Row>
-                    <Col size="md-12 white-background">
-                        <Jumbotron>
-                            <h1 className="text-left">
-                                <strong><i>Sign Up</i></strong>
-                            </h1>
-                        </Jumbotron>
-                    </Col>
-                </Row>
-            </Container>
+            <form onSubmit={this.onSubmit}>
+            <div className="form-group">
+                <input
+                    className="form-control"
+                    name="username"
+                    value={username}
+                    onChange={this.onChange}
+                    type="text"
+                    placeholder="Full Name"
+                />
+                </div>
+
+                <div className="form-group">
+                <input
+                    className="form-control"
+                    name="email"
+                    value={email}
+                    onChange={this.onChange}
+                    type="text"
+                    placeholder="Email Address"
+                />
+                </div>
+
+                <div className="form-group">
+                <input
+                    className="form-control"
+                    name="passwordOne"
+                    value={passwordOne}
+                    onChange={this.onChange}
+                    type="password"
+                    placeholder="Password"
+                />
+                </div>
+
+                <div className="form-group">
+                <input
+                    className="form-control"
+                    name="passwordTwo"
+                    value={passwordTwo}
+                    onChange={this.onChange}
+                    type="password"
+                    placeholder="Confirm Password"
+                />
+                </div>
+
+                <button
+                    className="btn btn-dark" 
+                    disabled={isInvalid} 
+                    type="submit">
+                    Ready!
+                    </button>
+
+                {error && <p>{error.message}</p>}
+
+            </form>
         );
     }
 }
 
-export default SignUp;
+const SignUpLink = () => (
+    <p>
+        Don't have an account? <Link to={ROUTES.SIGN_UP}>Sign Up</Link>
+    </p>
+);
+
+const SignUpForm = compose(
+    withRouter,
+    withFirebase,
+)(SignUpFormBase);
+
+export default SignUpPage;
+
+export { SignUpForm, SignUpLink };
